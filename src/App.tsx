1a68f80014867ee9bcf01d12a433ebf347375a51
andrei <andrei29.cervantes@gmail.com>
2026-02-16 16:42:01 +0800

    chore: setup shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
   return (
     <div>
       <h1 className="text-red-500 italic underline text-2xl">Hello World</h1>
+      <Button variant="default">Click me</Button>
     </div>
   );
 };
